8a12bcb579b20a109fa675f3843f79523034c395
DylanBehetre <dylan.behetre@outlook.fr>
2017-05-19 11:44:39 +0200

    Commencement IHMTextuelle

--- src/ihm/IHMTextuelle.java
+++ src/ihm/IHMTextuelle.java
@@ -2,16 +2,30 @@ package ihm;
 
 public class IHMTextuelle implements IHM {
     private String zoneDeTravail;
-    private boolean is
+    private boolean estLance;
 
     public IHMTextuelle(){
         this.zoneDeTravail = "";
+        this.estLance = false;
 
     }
 
     public void lancer(){
-        while(){
+        this.estLance = true;
+        while(this.estLance){
+            System.out.println("-----Espace de Travail-----");
+            System.out.println(this.zoneDeTravail);
+            System.out.println("-----Fin de l'espace de travail-----");
 
         }
+    }
+
+    private void afficherMenu(){
+        System.out.println("-----Menu-----");
+        System.out.println("Taper la commande souhaitez : ");
+        System.out.println("0-Selectionner");
+        System.out.println("-----Fin Menu-----");
+
+
     }
 }
