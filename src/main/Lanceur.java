14db9a5fb165e9e0016e2a7dd8ea73642c35cbc0
DylanBehetre <dylan.behetre@outlook.fr>
2017-05-19 13:50:18 +0200

    suppression majuscule methode interface

--- src/main/Lanceur.java
+++ src/main/Lanceur.java
@@ -1,7 +1,12 @@
 package main;
 
+import ihm.IHM;
+import ihm.IHMTextuelle;
+
 public class Lanceur {
     public static void main (String args[]){
+        IHM monIHM = new IHMTextuelle();
+        monIHM.lancer();
 
     }
 }
