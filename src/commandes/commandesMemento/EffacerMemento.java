b0db49713c6116c45ae42f380ebe4df200ee29c4
DylanBehetre <dylan.behetre@outlook.fr>
2017-05-26 18:58:29 +0200

    Refacto commande

--- src/commandes/commandesMemento/EffacerMemento.java
+++ src/commandes/commandesMemento/EffacerMemento.java
@@ -11,7 +11,7 @@ public class EffacerMemento implements CommandeMemento {
     private Commande effacerIntelligement;
 
     /*Constructors*/
-    public void EffacerMemento() {
+    public EffacerMemento() {
         this.effacerIntelligement = new EffacerIntelligemment();
     }
 
